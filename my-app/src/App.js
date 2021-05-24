62971dcfc76b202bda3cb1fe09dd15bd43fb0416
af9593 <moustafaallouhaibi@gmail.com>
2021-05-25 00:14:13 +0200

    implemented functionality for 0.1 user should be able to search for city

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import CitySearch from './CitySearch'
 
 function App() {
+  function addCity(city){
+    console.log(city)
+  }
   return (
     <div className="App">
       <h1> Weather map</h1>
-      <CitySearch/>
+      <CitySearch addCity = {addCity}/>
     </div>
   );
 }
